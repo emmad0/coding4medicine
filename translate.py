4119c19bd47299a9bdaa5b88c25490920797891d
emma <paphadadailey@gmail.com>
2026-07-08 12:27:07 -0700

    translate neucleotides to amino acids

---
+++ translate.py
@@ -0,0 +1,45 @@
+f = open("/share/SARS-class/SARS-2020.fasta")
+
+lines = f.readlines()
+
+seq = ""
+
+for i in range(1,len(lines)):
+    seq += lines[i].rstrip('\n')
+
+def translation(seq, start):
+    seqlen = range(start,len(seq),3)
+
+    aminoacids = {
+    'ATA':'I', 'ATC':'I', 'ATT':'I', 'ATG':'M',
+    'ACA':'T', 'ACC':'T', 'ACG':'T', 'ACT':'T',
+    'AAC':'N', 'AAT':'N', 'AAA':'K', 'AAG':'K',
+    'AGC':'S', 'AGT':'S', 'AGA':'R', 'AGG':'R',
+    'CTA':'L', 'CTC':'L', 'CTG':'L', 'CTT':'L',
+    'CCA':'P', 'CCC':'P', 'CCG':'P', 'CCT':'P',
+    'CAC':'H', 'CAT':'H', 'CAA':'Q', 'CAG':'Q',
+    'CGA':'R', 'CGC':'R', 'CGG':'R', 'CGT':'R',
+    'GTA':'V', 'GTC':'V', 'GTG':'V', 'GTT':'V',
+    'GCA':'A', 'GCC':'A', 'GCG':'A', 'GCT':'A',
+    'GAC':'D', 'GAT':'D', 'GAA':'E', 'GAG':'E',
+    'GGA':'G', 'GGC':'G', 'GGG':'G', 'GGT':'G',
+    'TCA':'S', 'TCC':'S', 'TCG':'S', 'TCT':'S',
+    'TTC':'F', 'TTT':'F', 'TTA':'L', 'TTG':'L',
+    'TAC':'Y', 'TAT':'Y', 'TAA':'*', 'TAG':'*',
+    'TGC':'C', 'TGT':'C', 'TGA':'*', 'TGG':'W'}
+
+    translation = ""
+
+    for i in seqlen:
+        if i > len(seq)-3:
+            break
+        else:
+            translation += aminoacids[seq[i:i+3]]
+
+    return translation
+
+print(translation(seq, 0))
+print()
+print(translation(seq, 1))
+print()
+print(translation(seq, 2))
